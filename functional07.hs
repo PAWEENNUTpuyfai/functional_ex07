a7bc9cf466a2592c6dd1178e310b480183cffd03
PAWEENNUTpuyfai <110964154+PAWEENNUTpuyfai@users.noreply.github.com>
2024-12-06 14:36:37 +0700

    Update functional07.hs

--- functional07.hs
+++ functional07.hs
@@ -8,11 +8,11 @@ con1 =  (any) . (<)
 contains2 :: (Foldable t, Ord a) => t a -> a -> Bool
 contains2 = \l x -> any (x<) l
 con2 :: (Foldable t, Ord a) => t a -> a -> Bool
-con2 =flip ((.) any (<))
+con2 =flip ((any) . (<))
 
 --write function len_comp that uses list comprehension to compute the length of the given list
 len_comp :: Num a => [t] -> a
-len_comp l = sum[1| x <- l]
+len_comp l = sum[1 | x <- l]
 
 -- rewrite [(x,y) | x <- [2,3,5], y <- [1,2,4], even $ x+y] without using list comprehension
 res = filter (even . uncurry (+)) $ concat $ map (\x -> map (x,) [1, 2, 4]) [2, 3, 5]
