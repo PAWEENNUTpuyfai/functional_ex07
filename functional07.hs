4b8eeee6fd8a4a2eda00b6cba750368500804b31
PAWEENNUTpuyfai <110964154+PAWEENNUTpuyfai@users.noreply.github.com>
2024-12-06 14:30:25 +0700

    Update functional07.hs

--- functional07.hs
+++ functional07.hs
@@ -12,7 +12,7 @@ con2 =flip ((.) any (<))
 
 --write function len_comp that uses list comprehension to compute the length of the given list
 len_comp :: Num a => [t] -> a
-len_comp ls = sum[1| x <- ls]
+len_comp l = sum[1| x <- l]
 
 -- rewrite [(x,y) | x <- [2,3,5], y <- [1,2,4], even $ x+y] without using list comprehension
 res = filter (even . uncurry (+)) $ concat $ map (\x -> map (x,) [1, 2, 4]) [2, 3, 5]
